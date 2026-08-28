545ff778b10b1d1b86a15c7b14e4a8d09b6b8d74
ProfFmendez <franciscometfg@gmail.com>
2026-08-27 22:50:19 -0600

    Prueba de taller
    Código base

---
+++ Taller/src/taller/Taller.java
@@ -0,0 +1,16 @@
+package taller;
+
+/**
+ *
+ * @author franc
+ */
+public class Taller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola a todos! :D");
+    }
+    
+}
