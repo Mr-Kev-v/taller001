db98ad96de1444992f1397b3647f383f93f76e32
ProfFmendez <franciscometfg@gmail.com>
2026-09-08 13:19:33 -0600

    Add ObjetoPR class and update .gitignore
    Adds Taller/src/taller/ObjetoPR.java: a simple Java class with a String attribute, constructor, toString implementation, and a placeholder metodoParaPR(). Updates .gitignore to ignore Taller/dist and Taller/build directories.

---
+++ Taller/src/taller/ObjetoPR.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller;
+
+/**
+ *
+ * @author franc
+ */
+public class ObjetoPR {
+    
+    private String atributo;
+    
+    public ObjetoPR(String atributo){
+        this.atributo = atributo;
+    }
+    
+    public String toString(){
+        return "{Objeto PR [ attributo: " + this.atributo + " ] }";
+    }
+    
+    public void metodoParaPR(){
+        //TODO: hagan código aquí :)
+    }
+    
+}
